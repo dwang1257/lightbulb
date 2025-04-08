510a2eac0f73df5b5e0883db739d06bcf99dc766
Dylan Wang <97680620+dwang1257@users.noreply.github.com>
2025-04-08 13:00:07 -0400

    changed geist font

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,15 +1,25 @@
 import type { Metadata } from "next";
+import localFont from 'next/font/local';
 import "./globals.css";
 import React from "react";  
-import Link from "next/link";
-import Image from "next/image";
-import lightbulb from "./lightbulb.png";
-import { geistSans, geistMono } from './fonts';
+
+
+export const geistSans = localFont({
+  src: './fonts/Geist-VariableFont_wght.ttf',
+  variable: '--font-geist-sans',
+  display: 'swap',
+});
+
+export const geistMono = localFont({
+  src: './fonts/GeistMono-VariableFont_wght.ttf',
+  variable: '--font-geist-mono',
+  display: 'swap',
+}); 
 
 export default function Layout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistMono.variable} font-sans antialiased`}>
         <main>
           {children}
         </main>
